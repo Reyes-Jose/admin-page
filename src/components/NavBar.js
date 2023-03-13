1cb62003e9e54f943537cc537b05229bac6415ac
Reyes-Jose <reyesj1209@gmail.com>
2023-03-13 16:25:45 -0700

    adds icon, gives error when importing icons. need to check.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './NavBarStyle.css'
+import { NotificationsNoneIcon } from '@mui/icons-material';
+//check icon error
+
 
 export default function NavBar() {
   return (
@@ -9,6 +12,9 @@ export default function NavBar() {
             <div className='adminLogo'>Admin</div>
         </div>
         <div className='navBarRight'>Right</div>
+        <div className='iconContainter'>
+        <NotificationsNoneIcon/>
+        </div>
         </div>
     </div>
   )
